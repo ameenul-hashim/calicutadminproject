1edcf933f404105d9632ac42010cdec3947e3ea4
ameenul hashim <hashimhusna1234@gmail.com>
2026-05-03 16:02:13 +0530

    security: Implement global PreventBackCacheMiddleware to disable browser caching after logout

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,14 +1,14 @@
-from django.utils.deprecation import MiddlewareMixin
+from django.utils.cache import add_never_cache_headers
 
-class PreventBackCacheMiddleware(MiddlewareMixin):
-    """
-    Middleware to prevent the browser from caching pages.
-    This ensures that when a user logs out and clicks the back button,
-    they don't see the previous authenticated page.
-    """
-    def process_response(self, request, response):
-        # Set headers to prevent caching
-        response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+class PreventBackCacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        # Add headers to prevent caching for all responses
+        # Cache-Control: no-cache, no-store, must-revalidate, max-age=0
+        add_never_cache_headers(response)
         response['Pragma'] = 'no-cache'
         response['Expires'] = '0'
         return response
