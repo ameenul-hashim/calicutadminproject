4186fbb1257c00d2d729a6dedd10487e3ba5b8b0
ameenul hashim <hashimhusna1234@gmail.com>
2026-05-04 19:12:40 +0530

    Deployment Fix: Reordered ASGI imports and added django.setup() to resolve ImproperlyConfigured error on startup.

--- elearning_project/asgi.py
+++ elearning_project/asgi.py
@@ -1,10 +1,14 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import accounts.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning_project.settings')
+django.setup()
+
+# Import routing AFTER django.setup()
+import accounts.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
